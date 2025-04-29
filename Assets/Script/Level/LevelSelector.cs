38b2646f27b1021c9f83f0bde28172c7bc5288c7
victor.simon <vic.simonpro@gmail.com>
2025-04-29 16:32:04 +0200

    :truck: move script + add bg

--- Assets/Script/Level/LevelSelector.cs
+++ Assets/Script/Level/LevelSelector.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
     public string levelName;
+    public Color color;
 
     private void Start()
     {
         TextMeshProUGUI txt = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         txt.text = levelName;
+        transform.GetComponent<Image>().color = color;
     }
     public void LoadLevel()
     {
